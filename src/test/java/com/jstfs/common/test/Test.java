6e8378a5638d08d12419fb5ee3a110a558ea923a
jstfs <lingxiaoshe@163.com>
2020-05-20 22:13:48 +0800

    test 'jstfs-loc' plugin

--- src/test/java/com/jstfs/common/test/Test.java
+++ src/test/java/com/jstfs/common/test/Test.java
@@ -1,5 +1,12 @@
 package com.jstfs.common.test;
 
-public class Test {
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
+public class Test {
+	public static void main(String[] args) {
+		SimpleDateFormat df1 = new SimpleDateFormat("yyyy-MM-dd'测试'HH:mm:ss.S"); 
+		String str = df1.format(new Date());
+		System.out.println(str);
+	}
 }
