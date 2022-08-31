14ba9768601a62b181ebf5535fdaccf244c2c624
jstfs <lingxiaoshe@163.com>
2022-08-31 19:20:55 +0800

    注释优化

--- src/main/java/com/jstfs/common/utils/MyRandomUtils.java
+++ src/main/java/com/jstfs/common/utils/MyRandomUtils.java
@@ -11,9 +11,9 @@ public class MyRandomUtils extends org.apache.commons.lang3.RandomUtils {
 	}
 	
 	/**
-	 * 生成一个指定大小(size)的int数组
-	 * 最小值: 1
-	 * 最大值: size
+	 * 生成一个指定大小的int数组,最小值为1,最大值为size
+	 * 
+	 * @param size	数组大小
 	 */
 	public static int[] generateIntAry(int size) {
 		int[] ary = new int[size];
@@ -27,21 +27,34 @@ public class MyRandomUtils extends org.apache.commons.lang3.RandomUtils {
 	}
 	
 	/**
-	 * 生成一个指定大小(size)的Integer数组
+	 * 
+	 * 
 	 * 最小值: 1
 	 * 最大值: size
 	 */
-	public static Integer[] generateIntegerAry(int size) {
-		Integer[] ary = new Integer[size];
-		int[] intAry = generateIntAry(size);
-		for(int i = 0; i < intAry.length; i++) {
-			ary[i] = intAry[i];
-		}
+	/**
+	 * 生成一个指定大小的int数组,最小值为1,最大值可以指定
+	 * 
+	 * @param size	数组大小
+	 * @param max	最大值
+	 */
+	public static int[] generateIntAry(int size, int max) {
+		int[] ary = new int[size];
+		int index = 0;
+		do {
+			int r = nextInteger(max);
+			ary[index++] = r;
+		} while(index < size);
+		
 		return ary;
 	}
 	
 	/**
-	 * 生成一个指定大小(size)和指定最小值和最大值的int数组
+	 * 生成一个指定大小的int数组,最小值为和最大值可以指定
+	 * 
+	 * @param size	数组大小
+	 * @param min	最小值
+	 * @param max	最大值
 	 */
 	public static int[] generateIntAry(int size, int min, int max) {
 		int[] ary = new int[size];
@@ -56,7 +69,25 @@ public class MyRandomUtils extends org.apache.commons.lang3.RandomUtils {
 	}
 	
 	/**
-	 * 生成一个指定大小(size)和指定最小值和最大值的Integer数组
+	 * 生成一个指定大小的Integer数组,最小值为1,最大值为size
+	 * 
+	 * @param size	数组大小
+	 */
+	public static Integer[] generateIntegerAry(int size) {
+		Integer[] ary = new Integer[size];
+		int[] intAry = generateIntAry(size);
+		for(int i = 0; i < intAry.length; i++) {
+			ary[i] = intAry[i];
+		}
+		return ary;
+	}
+	
+	/**
+	 * 生成一个指定大小的Integer数组,最小值为和最大值可以指定
+	 * 
+	 * @param size	数组大小
+	 * @param min	最小值
+	 * @param max	最大值
 	 */
 	public static Integer[] generateIntegerAry(int size, int min, int max) {
 		Integer[] ary = new Integer[size];
@@ -69,12 +100,15 @@ public class MyRandomUtils extends org.apache.commons.lang3.RandomUtils {
 	
 	/**
 	 * 生成指定大小的指定长度手机号数组
+	 * 
+	 * @param size			数组大小
+	 * @param phoneNoLength	手机号长度
 	 */
-	public static String[] generatePhoneNo(int size, int dataLength) {
+	public static String[] generatePhoneNo(int size, int phoneNoLength) {
 		String[] phoneNos = new String[size];
 		for(int i = 0; i < size; i++) {
 			StringBuffer phoneNo = new StringBuffer("1");	//第一位都是1
-			for(int j = 0; j < dataLength - 1; j++) {
+			for(int j = 0; j < phoneNoLength - 1; j++) {
 				int bit = nextInteger(9);
 				phoneNo.append(bit);
 			}
