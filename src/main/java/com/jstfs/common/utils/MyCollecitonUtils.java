f39507437f9dade17bb38c9414bb5d9952559a35
jstfs <lingxiaoshe@163.com>
2022-09-01 02:35:47 +0800

    增加工具类

---
+++ src/main/java/com/jstfs/common/utils/MyCollecitonUtils.java
@@ -0,0 +1,12 @@
+package com.jstfs.common.utils;
+
+import org.apache.commons.collections.CollectionUtils;
+
+/**
+ * 
+ * @createBy	落叶
+ * @createTime	2022年9月1日 上午2:16:58
+ */
+public class MyCollecitonUtils extends CollectionUtils {
+
+}
---
+++ src/main/java/com/jstfs/common/utils/MyStringUtils.java
@@ -0,0 +1,12 @@
+package com.jstfs.common.utils;
+
+import org.apache.commons.lang3.StringUtils;
+
+/**
+ * 
+ * @createBy	落叶
+ * @createTime	2022年9月1日 上午12:25:40
+ */
+public class MyStringUtils extends StringUtils {
+
+}
